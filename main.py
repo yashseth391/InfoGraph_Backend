a1b6981a30ab85a81e96f8a5d46a3b22709edc93
Anjith Reddy <anjithreddy5@gmail.com>
2025-01-16 19:43:15 +0530

    changes to first infograph

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def draw_text_lines(startX, startY, endX, endY, lines, font, image):
     draw = ImageDraw.Draw(image)
     y_position = startY
     max_width = endX - startX
-    wrapped_lines = wrap(lines, width=80)  # Adjust width as needed
+    wrapped_lines = wrap(lines, width=80)
 
     for line in wrapped_lines:
         text_bbox = draw.textbbox((0, 0), line, font=font)
@@ -26,11 +26,11 @@ def draw_text_lines(startX, startY, endX, endY, lines, font, image):
                 sub_text_bbox = draw.textbbox((0, 0), sub_line, font=font)
                 sub_text_width = sub_text_bbox[2] - sub_text_bbox[0]
                 x_position = startX + (max_width - sub_text_width) // 2
-                draw.text((x_position, y_position), sub_line, fill="#FFFFFF", font=font)
+                draw.text((x_position, y_position), sub_line, fill="#000000", font=font)
                 y_position += text_height + 10
         else:
             x_position = startX + (max_width - text_width) // 2
-            draw.text((x_position, y_position), line, fill="#FFFFFF", font=font)
+            draw.text((x_position, y_position), line, fill="#000000", font=font)
             y_position += text_height + 10
 
         if y_position > endY:
@@ -38,138 +38,100 @@ def draw_text_lines(startX, startY, endX, endY, lines, font, image):
 
     return y_position
 
-app.mount("/imgs", StaticFiles(directory="imgs"), name='images')
-
-@app.get("/", response_class=HTMLResponse)
-def serve():
-    logging.basicConfig(level=logging.INFO)
-    img = Image.open('./imgs/images.png')
-    I1 = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Roboto-Regular.ttf", 200)
-    I1.text((28, 36), "nice Car", fill=(0, 0, 0), font=font)
-    img.save('./imgs/edited_image.png')
-    img = Image.open('./imgs/edited_image.png')
-    img.show()
 
 @app.get("/trends", response_class=HTMLResponse)
-async def overlay_image(request: Request):
+async def overlay_image_1(request: Request):
     try:
-        logging.basicConfig(level=logging.INFO)
+        heading = request.query_params.get('heading', 'Understanding Global Warming: Causes, Effects, and Future Projections')
+        SubHeading1 = request.query_params.get('SubHeading1', 'Researchers achieve significant progress in natural language processing')
+        Desription1 = request.query_params.get('Desription1', 'Global warming is primarily driven by human activities, namely the burning of fossil fuels, deforestation, and certain agricultural practices. These activities release greenhouse gases like carbon dioxide, methane, and nitrous oxide into the atmosphere, which trap heat and lead to an increase in Earth s average surface temperature.')
+        SubHeading2 = request.query_params.get('SubHeading2', 'Current and Projected Temperature Increases')
+        Desription2 = request.query_params.get('Desription2', 'Since the pre-industrial period, Earth s average temperature has risen by approximately 1 degree Celsius. Currently, the temperature is increasing at a rate of more than 0.2 degrees Celsius per decade. The World Meteorological Organization predicts an 80% chance that global temperatures will surpass 1.5 °C for at least one year between 2024 and 2028.')
+        SubHeading3 = request.query_params.get('SubHeading3', 'Impacts of Global Warming')
+        Desription3 = request.query_params.get('Desription3', 'The effects of global warming include significant phenomena such as sea ice loss, melting glaciers, rising sea levels, and extreme weather events like heat waves, droughts, and wildfires. Future scenarios under high emission conditions predict even more severe weather patterns and substantial changes to land surfaces, including desertification and loss of agricultural land.')
+        SubHeading4 = request.query_params.get('SubHeading4', 'Mitigation and Long-term Consequences')
+        Desription4 = request.query_params.get('Desription4', 'To limit global warming to 1.5°C, it is essential to halve greenhouse gas emissions by 2030 and achieve net-zero emissions by 2050. Even with successful mitigation efforts, some effects, like ocean heating and acidification, will persist for centuries, emphasizing the need for immediate global action to ensure a sustainable future.')
+        #SubHeading5 = request.query_params.get('SubHeading5', 'Researchers achieve significant progress in natural language processing')
+        #Desription5 = request.query_params.get('Desription5', '')
+        heading_color = request.query_params.get('heading_color', 150)
+        sub_heading_color = request.query_params.get('sub_heading_color', 80)
+        description_color = request.query_params.get('description_color', 50)
+        image_url = request.query_params.get('image_url', 'imgs/temp_5_steps_litz.jpg')
         
-        y_position = 0
-        text = request.query_params.get('text', 'New AI Breakthrough')
-        subtext = request.query_params.get('subtext', 'Researchers achieve significant progress in natural language processing')
-        funding_amount = request.query_params.get('funding_amount', '')
-        website_url = request.query_params.get('website_url', '')
-        logo_url = request.query_params.get('logo_url', '')
-        
-       # image_url = "https://iili.io/2470hrl.png"
-        image_url = "https://iili.io/26V1t8G.png"
-        
-        response = requests.get(image_url)
-        image = Image.open(BytesIO(response.content))
+        if image_url.startswith("http"):
+            # If the image URL is an HTTP link, download it
+            response = requests.get(image_url)
+            image = Image.open(BytesIO(response.content))
+        else:
+            # Otherwise, treat it as a local file path
+            image = Image.open(image_url)
+
         image = image.convert('RGB')
-        draw = ImageDraw.Draw(image)
-        
-        
-        
-        heading='Understanding Global Warming: Causes, Effects, and Future Projections'
-        SubHeading1=' Primary Causes of Global Warming'
-        Desription1= 'Global warming is primarily driven by human activities, namely the burning of fossil fuels, deforestation, and certain agricultural practices. These activities release greenhouse gases like carbon dioxide, methane, and nitrous oxide into the atmosphere, which trap heat and lead to an increase in Earth s average surface temperature.'
-        SubHeading2= 'Current and Projected Temperature Increases'
-        Description2= 'Since the pre-industrial period, Earth s average temperature has risen by approximately 1 degree Celsius. Currently, the temperature is increasing at a rate of more than 0.2 degrees Celsius per decade. The World Meteorological Organization predicts an 80% chance that global temperatures will surpass 1.5 °C for at least one year between 2024 and 2028.'
-        SubHeading3= 'Impacts of Global Warming'
-        Descrption3= 'The effects of global warming include significant phenomena such as sea ice loss, melting glaciers, rising sea levels, and extreme weather events like heat waves, droughts, and wildfires. Future scenarios under high emission conditions predict even more severe weather patterns and substantial changes to land surfaces, including desertification and loss of agricultural land.'
-        SubHeading4=' Mitigation and Long-term Consequences'
-        Descrption4= 'To limit global warming to 1.5°C, it is essential to halve greenhouse gas emissions by 2030 and achieve net-zero emissions by 2050. Even with successful mitigation efforts, some effects, like ocean heating and acidification, will persist for centuries, emphasizing the need for immediate global action to ensure a sustainable future.'
-        
-    #Loading Fonts
+
         try:
-            headingFont = ImageFont.truetype('WorkSans-Bold.ttf', 50)
-            SubHeadingFont=ImageFont.truetype('WorkSans-Bold.ttf', 24)
-            descriptionFont=ImageFont.truetype('WorkSans-Regular.ttf',14)
-            comfortaa_font_small = ImageFont.truetype('WorkSans-Regular.ttf', 32)
+            headingFont = ImageFont.truetype('fonts/Acme.woff2', heading_color)
+            SubHeadingFont=ImageFont.truetype('fonts/Acme.woff2', sub_heading_color)
+            descriptionFont=ImageFont.truetype('fonts/Acme.woff2',description_color)
         except IOError:
-            logging.warning("Failed to load custom fonts, using default font.")
             headingFont = ImageFont.load_default()
             SubHeadingFont = ImageFont.load_default()
             descriptionFont = ImageFont.load_default()
-            comfortaa_font_small = ImageFont.load_default()
 
-        
-        # draw.text((28, 36), heading, fill=(0, 0, 0), font=comfortaa_font_large)
-        
-          # Coordinates of the blue boxes
-       # Coordinates of the corners of the rectangular box
+
         top_left = (1.33*144, 1.31*345)
-        top_right = (1.33*473, 1.31*345)
-        bottom_left = (1.33*144, 1.31*510)
-        bottom_right = (1.33*473, 1.31*510)
+        top_right = (5*473, 6*1000)
+        #bottom_left = (1.33*144, 1.31*510)
+        #bottom_right = (1.33*473, 1.31*510)
          # Calculate the center of the rectangular box
-        center_x = (top_left[0] + top_right[0] + bottom_left[0] + bottom_right[0]) // 4
-        center_y = (top_left[1] + top_right[1] + bottom_left[1] + bottom_right[1]) // 4
+        #center_x = (top_left[0] + top_right[0] + bottom_left[0] + bottom_right[0]) // 4
+        #center_y = (top_left[1] + top_right[1] + bottom_left[1] + bottom_right[1]) // 4
         
         # main_lines = wrap(heading, width=30)
         #Main Lines
-        draw_text_lines(50,50,image.size[0]-30,image.size[0]-50,heading,headingFont,image)
+        draw_text_lines(200,200,image.size[0]-200,image.size[0]-200,heading,headingFont,image)
         
         # # Calculate the width of the rectangular box
-        startX = top_left[0]
-        startY = top_left[1]
-        endX = top_right[0]
-        endY = top_right[1]
-        # box_height = bottom_left[1] - top_left[1]
-        # box_width = (top_right[0] + top_left[0]) // 2
-        
-        
+        startX = 0#top_left[0]
+        startY = 0#top_left[1]
+        endX = 2500#top_right[0]
+        endY = 6000#top_right[1]
+                
         #SubHeading1
-        passingY=draw_text_lines(startX+70,startY-10,endX,endY,SubHeading1,SubHeadingFont,image)
-        draw_text_lines(startX+70,passingY+20,endX,endY,Desription1,descriptionFont,image)
-        
-        
-        
+        passingY=draw_text_lines(startX+500,startY+1500,endX-300,endY+1000,SubHeading1,SubHeadingFont,image)
+        draw_text_lines(startX+500,passingY+100,endX-300,endY,Desription1,descriptionFont,image)
         
         
         #SubHeading2
-        passingY=draw_text_lines(startX+70,startY+360,endX,endY+340,SubHeading2,SubHeadingFont,image)
-        draw_text_lines(startX+70,passingY+20,endX,endY,Description2,descriptionFont,image)
+        passingY=draw_text_lines(startX+70,startY+2500,endX-600,endY,SubHeading2,SubHeadingFont,image)
+        draw_text_lines(startX+70,passingY+100,endX-600,endY,Desription2,descriptionFont,image)
        
         
-       #SubHeading3
-        passingY=draw_text_lines(startX+70,startY+720,endX,endY+720,SubHeading3,SubHeadingFont,image)
-        draw_text_lines(startX+70,passingY+20,endX,endY,Descrption3,descriptionFont,image)
+        #SubHeading3
+        passingY=draw_text_lines(startX+500,startY+3500,endX-300,endY,SubHeading3,SubHeadingFont,image)
+        draw_text_lines(startX+500,passingY+100,endX-300,endY,Desription3,descriptionFont,image)
 
         #SubHeading4
-        passingY=draw_text_lines(startX+70,startY+1090,endX,endY+1090,SubHeading4,SubHeadingFont,image)
-        draw_text_lines(startX+70,passingY+20,endX,endY,Descrption4,descriptionFont,image)
-          
+        passingY=draw_text_lines(startX+70,startY+4500,endX-600,endY,SubHeading4,SubHeadingFont,image)
+        draw_text_lines(startX+70,passingY+100,endX-600,endY,Desription4,descriptionFont,image)
+        
+        #SubHeading5
+        passingY=draw_text_lines(startX+500,startY+5500,endX-300,endY,SubHeading3,SubHeadingFont,image)
+        draw_text_lines(startX+500,passingY+100,endX-300,endY,Desription3,descriptionFont,image)
+
         
         image.save('./imgs/edited_image.png')
         image = Image.open('./imgs/edited_image.png')
         image.show()
-
-        # if website_url:
-        #     website_text = website_url
-        #     text_width, text_height = draw.textsize(website_text, font=comfortaa_font_small)
-        #     x_position = (image.size[0] - text_width) // 2
-        #     draw.text((x_position, y_position), website_text, fill="#FFFFFF", font=comfortaa_font_small)
-
-        # if funding_amount:
-        #     funding_text = "Funding: $ " + funding_amount
-        #     amount_y_position = image.size[1] - 300
-        #     text_width, text_height = draw.textsize(funding_text, font=comfortaa_font_large)
-        #     x_position = (image.size[0] - text_width) // 2
-        #     draw.text((x_position, amount_y_position), funding_text, fill="#FFFFFF", font=comfortaa_font_large)
-
+        return 
         img_byte_arr = BytesIO()
         image.save(img_byte_arr, format='JPEG')
         img_byte_arr.seek(0)
 
         img_base64 = base64.b64encode(img_byte_arr.getvalue()).decode('utf-8')
-
+        print("Test")
         FREEIMAGE_UPLOAD_URL = "https://api.imgbb.com/1/upload"
         params = {
-            "key": "daeb1bf2967b2875450375e2ee699c88",
+            "key": "11b1e818e3238dc36d4cc8e769e75da9",
             "image": img_base64
         }
         
@@ -179,6 +141,7 @@ async def overlay_image(request: Request):
             link = response.json()["data"]["url"]
             logging.info(f"Image uploaded successfully: {link}")
             image_url = link
+            return image_url
         else:
             error_message = response.json().get('error', {}).get('message', 'Unknown error')
             logging.error(f"Failed to upload image. Error: {error_message}")
@@ -186,14 +149,7 @@ async def overlay_image(request: Request):
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
-#     return f"""
-#     <html>
-#     <head>
-#         <title>Image with Text</title>
-#     </head>
-#     <body>
-#         <h1>Hello World</h1>
-#         <img src="{image_url}" alt="Edited Image" style="max-width: 100%; height: 50%;">
-#     </body>
-# </html>
-#     """
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5005)
